4b41de6e745ed77e631363789fd4c72768b0269f
Talant Ruzmetov <truzmeto@kent.edu>
2017-10-14 18:17:22 -0400

    implemented testing error calc

--- adult_test.py
+++ adult_test.py
@@ -21,26 +21,29 @@ import opt.SimulatedAnnealing as SimulatedAnnealing
 import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
 
 
-INPUT_FILE = os.path.join(".","clean_data","adult_train.txt")
+INPUT_FILE_train = os.path.join(".","clean_data","adult_train.txt")
+INPUT_FILE_test = os.path.join(".","clean_data","adult_test.txt")
+
 
 INPUT_LAYER = 11
 HIDDEN_LAYER = 5
 OUTPUT_LAYER = 1
-TRAINING_ITERATIONS = 30
+TRAINING_ITERATIONS = 10
 
-def initialize_instances():
+def initialize_instances(file_path):
     """Read the abalone.txt CSV data into a list of instances."""
     instances = []
-
-    # Read in the abalone.txt CSV file
-    with open(INPUT_FILE, "r") as abalone:
-        reader = csv.reader(abalone)
-
+    
+    # Read in the adult_train.txt CSV file
+    with open(file_path, "r") as adult:
+        reader = csv.reader(adult)
+           
         for row in reader:
             instance = Instance([float(value) for value in row[:-1]])
-            #instance.setLabel(Instance(0 if float(row[-1]) < 15 else 1))
-            #instances.append(instance)
-
+            # my data was already preprocessed, so this basically does nothing but appends my data to instances
+            instance.setLabel(Instance(0 if float(row[-1]) < 1 else 1))
+            instances.append(instance)
+                       
     return instances
 
 
@@ -68,12 +71,13 @@ def train(oa, network, oaName, instances, measure):
             example = Instance(output_values, Instance(output_values.get(0)))
             error += measure.value(output, example)
 
-        print "%0.03f" % error
+        print  round(error,3)
 
 
 def main():
     """Run algorithms on the abalone dataset."""
-    instances = initialize_instances()
+    instances = initialize_instances(INPUT_FILE_train)
+    instances_test = initialize_instances(INPUT_FILE_test)
     factory = BackPropagationNetworkFactory()
     measure = SumOfSquaresError()
     data_set = DataSet(instances)
@@ -105,14 +109,14 @@ def main():
         optimal_instance = oa[i].getOptimal()
         networks[i].setWeights(optimal_instance.getData())
 
+        # here we make make prediction on training set-------------------------------------------------------------------------------------------------
         start = time.time()
         for instance in instances:
             networks[i].setInputValues(instance.getData())
             networks[i].run()
-
-            predicted = instance.getLabel().getContinuous()
-            actual = networks[i].getOutputValues().get(0)
-
+            ## swapped actual and predicted from original version
+            actual = instance.getLabel().getContinuous()
+            predicted = networks[i].getOutputValues().get(0)
             if abs(predicted - actual) < 0.5:
                 correct += 1
             else:
@@ -121,11 +125,36 @@ def main():
         end = time.time()
         testing_time = end - start
 
+        results += "------------------------------- reporting on training set ---------------------------"
         results += "\nResults for %s: \nCorrectly classified %d instances." % (name, correct)
         results += "\nIncorrectly classified %d instances.\nPercent correctly classified: %0.03f%%" % (incorrect, float(correct)/(correct+incorrect)*100.0)
         results += "\nTraining time: %0.03f seconds" % (training_time,)
         results += "\nTesting time: %0.03f seconds\n" % (testing_time,)
+        
+        # here we make make prediction on test set-----------------------------------------------------------------------------------------------------
+        start = time.time()
+        correct = 0
+        incorrect = 0
+        for instance in instances_test:
+            networks[i].setInputValues(instance.getData())
+            networks[i].run()
+            ## swapped actual and predicted from original version
+            actual = instance.getLabel().getContinuous()
+            predicted = networks[i].getOutputValues().get(0)
+            if abs(predicted - actual) < 0.5:
+                correct += 1
+            else:
+                incorrect += 1
+                
+        end = time.time()
+        testing_time = end - start
 
+        results += "------------------------------- reporting on testing set ---------------------------"
+        results += "\nResults for %s: \nCorrectly classified %d instances." % (name, correct)
+        results += "\nIncorrectly classified %d instances.\nPercent correctly classified: %0.03f%%" % (incorrect, float(correct)/(correct+incorrect)*100.0)
+        results += "\nTesting time: %0.03f seconds\n" % (testing_time,)
+
+        
     print results
 
 
